10981941f0b8f998ae173449cbce988d1bfefa8c
Filip Rares <Rares.Filip@garmin.com>
2021-12-06 12:40:36 +0200

    Lab 7

--- Lab5/src/Item.java
+++ Lab5/src/Item.java
@@ -1,13 +1,12 @@
-
-
 import java.util.List;
+import java.util.Objects;
 
-public class Production {
+public class Item {
     private String leftHandSide;
     private List<String> rightHandSide;
     private int dotPosition;
 
-    public Production(String leftHandSide, List<String> rightHandSide) {
+    public Item(String leftHandSide, List<String> rightHandSide) {
         this.leftHandSide = leftHandSide;
         this.rightHandSide = rightHandSide;
         this.dotPosition = 0;
@@ -39,8 +38,21 @@ public class Production {
 
     @Override
     public String toString() {
-        String s="";
-        s+=this.leftHandSide+"->"+this.rightHandSide+", dot position:"+this.dotPosition;
+        String s = "";
+        s += this.leftHandSide + "->" + this.rightHandSide + ", dot position:" + this.dotPosition;
         return s;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return dotPosition == item.dotPosition && Objects.equals(leftHandSide, item.leftHandSide) && Objects.equals(rightHandSide, item.rightHandSide);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(leftHandSide, rightHandSide, dotPosition);
     }
 }
--- Lab5/src/LR0.java
+++ Lab5/src/LR0.java
@@ -11,27 +11,27 @@ public class LR0 {
         List<String> listStrings = new ArrayList<>(Collections.singletonList(this.grammar.S));
         List<List<String>> toBeAdded = new ArrayList<>();
         toBeAdded.add(listStrings);
-        this.starting=this.grammar.S;
+        this.starting = this.grammar.S;
         this.grammar.getP().put("S'", toBeAdded);
         this.grammar.S = "S'";
     }
 
-    public List<Production> closure(List<Production> productions) {
+    public List<Item> closure(List<Item> items) {
 
-        Queue<Production> queue = new LinkedList<>(productions);
-        List<Production> closure = new ArrayList<>(productions);
+        Queue<Item> queue = new LinkedList<>(items);
+        List<Item> closure = new ArrayList<>(items);
 
-        Production currentProduction;
+        Item currentItem;
         while (!queue.isEmpty()) {
-            currentProduction = queue.remove();
-            if (currentProduction.getDotPosition() < currentProduction.getRightHandSide().size()) {
-                String leftHandSide = currentProduction.getRightHandSide().get(currentProduction.getDotPosition());
+            currentItem = queue.remove();
+            if (currentItem.getDotPosition() < currentItem.getRightHandSide().size()) {
+                String leftHandSide = currentItem.getRightHandSide().get(currentItem.getDotPosition());
                 if (grammar.getN().contains(leftHandSide)) {
                     for (List<String> rightHandSide : grammar.P.get(leftHandSide)) {
-                        Production production = new Production(leftHandSide, rightHandSide);
-                        if (!checkContains(closure,production)) {
-                            closure.add(production);
-                            queue.add(production);
+                        Item item = new Item(leftHandSide, rightHandSide);
+                        if (!checkContains(closure, item)) {
+                            closure.add(item);
+                            queue.add(item);
                         }
                     }
                 }
@@ -40,11 +40,11 @@ public class LR0 {
         return closure;
     }
 
-    public List<Production> goTo(List<Production> state, String element) {
-        List<Production> newStates = new ArrayList<>();
-        for (Production p : state) {
+    public List<Item> goTo(List<Item> state, String element) {
+        List<Item> newStates = new ArrayList<>();
+        for (Item p : state) {
             if (p.getDotPosition() < p.getRightHandSide().size() && p.getRightHandSide().get(p.getDotPosition()).equals(element)) {
-                Production a = new Production(p.getLeftHandSide(), p.getRightHandSide());
+                Item a = new Item(p.getLeftHandSide(), p.getRightHandSide());
                 a.setDotPosition(p.getDotPosition() + 1);
                 newStates.add(a);
             }
@@ -53,31 +53,31 @@ public class LR0 {
     }
 
 
-    public List<List<Production>> canonicalCollection() {
-        List<List<Production>> canonicalCollection = new ArrayList<>();
+    public List<List<Item>> canonicalCollection() {
+        List<List<Item>> canonicalCollection = new ArrayList<>();
         List<String> rightHandSide = new ArrayList<>();
         rightHandSide.add(this.starting);
-        List<Production> productionsS0 = new ArrayList<>();
-        productionsS0.add(new Production("S'", rightHandSide));
-        List<Production> s0 = closure(productionsS0);
+        List<Item> productionsS0 = new ArrayList<>();
+        productionsS0.add(new Item("S'", rightHandSide));
+        List<Item> s0 = closure(productionsS0);
         canonicalCollection.add(s0);
         List<String> nue = new ArrayList<>();
         nue.addAll(this.grammar.getN());
         nue.addAll(this.grammar.getE());
-        List<List<Production>> canonicalCollectionCopy;
+        List<List<Item>> canonicalCollectionCopy;
         canonicalCollectionCopy = canonicalCollection.stream().collect(Collectors.toList());
         while (true) {
-            for (List<Production> p : canonicalCollection) {
+            for (List<Item> p : canonicalCollection) {
                 for (String n : nue) {
-                    List<Production> elem = goTo(p, n);
+                    List<Item> elem = goTo(p, n);
                     if (!elem.isEmpty() && !check(canonicalCollectionCopy, elem)) {
                         canonicalCollectionCopy.add(elem);
                     }
                 }
             }
             int ok = 1;
-            for (List<Production> productions : canonicalCollectionCopy) {
-                if (!check(canonicalCollection, productions)) {
+            for (List<Item> items : canonicalCollectionCopy) {
+                if (!check(canonicalCollection, items)) {
                     ok = 0;
                 }
             }
@@ -86,45 +86,108 @@ public class LR0 {
             }
             canonicalCollection = List.copyOf(canonicalCollectionCopy);
         }
+
         return canonicalCollection;
     }
 
-    public boolean check(List<List<Production>> list, List<Production> elem) {
+    public boolean check(List<List<Item>> list, List<Item> elem) {
         int ok, nr;
-        for (List<Production> productions : list) {
+        for (List<Item> items : list) {
             ok = 0;
             nr = 0;
-            for (Production production : productions) {
-                for (Production value : elem) {
-                    if (production.getLeftHandSide().equals(value.getLeftHandSide()) && production.getRightHandSide().equals(value.getRightHandSide()) && production.getDotPosition() == value.getDotPosition()) {
+            for (Item item : items) {
+                for (Item value : elem) {
+                    if (item.getLeftHandSide().equals(value.getLeftHandSide()) && item.getRightHandSide().equals(value.getRightHandSide()) && item.getDotPosition() == value.getDotPosition()) {
                         nr++;
                     }
                 }
             }
-            if (nr == productions.size()) {
-                return true;
-            }
-        }
-        return false;
-    }
-    private boolean checkContains(List<Production> productions, Production production) {
-        for (Production p : productions) {
-            if (p.getLeftHandSide().equals(production.getLeftHandSide()) && p.getRightHandSide().equals(production.getRightHandSide()) && p.getDotPosition()==production.getDotPosition()) {
+            if (nr == items.size()) {
                 return true;
             }
         }
         return false;
     }
 
-    public String stateToString(List<List<Production>> states)
-    {
-        String s="";
-        for(int i=0;i<states.size();i++) {
+    private boolean checkContains(List<Item> items, Item item) {
+        for (Item p : items) {
+            if (p.getLeftHandSide().equals(item.getLeftHandSide()) && p.getRightHandSide().equals(item.getRightHandSide()) && p.getDotPosition() == item.getDotPosition()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String stateToString(List<List<Item>> states) {
+        String s = "";
+        for (int i = 0; i < states.size(); i++) {
             s += "state " + i + " " + states.get(i);
-            s+="\n";
+            s += "\n";
         }
 
         return s;
+    }
+
+
+    public Map<List<String>, List<List<String>>> generateLR0Table(List<List<Item>> canonicalCollection) {
+        // Map<Pair<State,Action>,List<Pair<NUE,State>>>
+        Map<List<String>, List<List<String>>> lr0Table = new HashMap<>();
+        int reduceIndex = 1;
+
+        for (int i = 0; i < canonicalCollection.size(); i++) {
+            List<Item> items = canonicalCollection.get(i);
+            if (items.size() == 1 &&
+                    Objects.equals(items.get(0).getLeftHandSide(), "S'") &&
+                    Objects.equals(items.get(0).getRightHandSide().get(0), "S") &&
+                    items.get(0).getDotPosition() == 1) {
+                lr0Table.put(new ArrayList<>(Arrays.asList(String.valueOf(i), "Accept")), new ArrayList<>());
+            } else {
+                for (Item item : items) {
+                    if (item.getRightHandSide().size() == item.getDotPosition()) {
+                        lr0Table.put(
+                                new ArrayList<>(Arrays.asList(String.valueOf(i), "Reduce " + reduceIndex++)),
+                                new ArrayList<>()
+                        );
+                    }
+                }
+            }
+            if (allShift(items)) {
+                for (Item item : items) {
+                    var lmao = goTo(items, item.getRightHandSide().get(item.getDotPosition()));
+                    int size = -1;
+                    for (int k = 0; k < canonicalCollection.size(); k++) {
+                        if (checkContainsAll(canonicalCollection.get(k), lmao)) {
+                            size = k;
+                        }
+                    }
+                    System.out.println(size);
+                    lr0Table.put(
+                            new ArrayList<>(Arrays.asList(String.valueOf(i), "Shift")),
+                            new ArrayList<>()
+                    );
+                }
+            }
+//            System.out.println(allShift(productions));
+        }
+        return lr0Table;
+    }
+
+    private boolean checkContainsAll(List<Item> state, List<Item> goToResult) {
+        for (int i = 0; i < state.size(); i++) {
+            if (!state.get(i).equals(goToResult.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean allShift(List<Item> items) {
+        for (Item item : items) {
+            if (item.getDotPosition() == item.getRightHandSide().size()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
